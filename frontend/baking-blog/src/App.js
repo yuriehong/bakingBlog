714cfa8b5f109c7db429ca3c2876b2d628512b8e
+ <+>
2023-03-28 17:09:52 -0400

    frontend

---
+++ frontend/baking-blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Baking Blog
+      </header>
+    </div>
+  );
+}
+
+export default App;
